a58ef1836456ce4addbc5255c5d748dab302b0c5
Raimundo Alexandre <alexandre.fsantos08@gmail.com>
2025-11-17 21:06:09 -0300

    first

--- conversor_de_moedas/src/Project.java
+++ conversor_de_moedas/src/Project.java
@@ -1,9 +1,18 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Scanner;
 
 public class Project {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String apiKey = "7c893e5f28efb48fcbd047d5";
+        String apiKey = "4688f0854b8e71ecffe9d0fa";
         System.out.println("===== CONVERSOR DE MOEDAS =====");
         System.out.println("Moedas disponíveis:");
         System.out.println("1 - ARS (Peso Argentino)");
@@ -22,5 +31,28 @@ public class Project {
         String[] moedas = new String[]{"ARS", "BOB", "BRL", "CLP", "COP", "USD"};
         String moedaOrigem = moedas[opcaoOrigem - 1];
         String moedaDestino = moedas[opcaoDestino - 1];
+
+        try {
+            String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + moedaOrigem;
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            JsonElement elemento = JsonParser.parseString((String)response.body());
+            JsonObject json = elemento.getAsJsonObject();
+            JsonObject rates = json.getAsJsonObject("conversion_rates");
+            double taxa = rates.get(moedaDestino).getAsDouble();
+            double convertido = valor * taxa;
+            System.out.println("\n===== RESULTADO =====");
+            System.out.println("De: " + moedaOrigem);
+            System.out.println("Para: " + moedaDestino);
+            System.out.println("Valor inserido: " + valor);
+            System.out.println("Taxa da API: " + taxa);
+            System.out.println("Valor convertido: " + convertido);
+            System.out.println("======================");
+        } catch (Exception e) {
+            System.out.println("Erro ao consultar API: " + e.getMessage());
+        }
+
+        sc.close();
     }
 }
---
+++ conversor_de_moedas/src/jdk/text/JsonObject.java
@@ -0,0 +1,4 @@
+package jdk.text;
+
+public class JsonObject {
+}
